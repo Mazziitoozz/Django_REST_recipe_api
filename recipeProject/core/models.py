a3964b38192910ade43b9dfb069c0081ff8ea7e8
Jack Sparrow <mazziitoozz@gmail.com>
2020-01-29 12:31:32 +0000

    lecture 32

--- recipeProject/core/models.py
+++ recipeProject/core/models.py
@@ -14,8 +14,15 @@ class UserManager(BaseUserManager):
 
         return user
 
+    def create_superuser(self,email,password):
+        # Create and saves and new superuser
+        user =self.create_user(email,password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self.db)
+        return user
 class User(AbstractBaseUser,PermissionsMixin):
-    # Custo user model that supports using email instead of username
+    # Custom user model that supports using email instead of username
     email = models.EmailField(max_length=255, unique = True)
     name = models.CharField(max_length =255)
     is_active = models.BooleanField(default=True)
