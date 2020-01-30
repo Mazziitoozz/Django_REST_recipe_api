9df0540b1f2ff0d040b085da0ac186b96e789789
Jack Sparrow <mazziitoozz@gmail.com>
2020-01-30 11:31:27 +0000

    Custom admin page

--- recipeProject/core/tests/test_admin.py
+++ recipeProject/core/tests/test_admin.py
@@ -24,3 +24,17 @@ class AdminSiteTest(TestCase):
 
         self.assertContains(res,self.user.name)
         self.assertContains(res,self.user.email)
+
+    def test_user_change_page(self):
+        # Test that the user edit page works
+        url = reverse('admin:core_user_change',args = [self.user.id]) # /admin/core/user/id
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code,200)   #  Http 200 means that the page it is ok
+    
+    def test_create_user_page(self):
+        # Test htat the create user page works
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code,200)   #  Http 200 means that the page it is ok
